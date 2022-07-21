65b55875abcd1b1d0efe334da728bfc5b77d169e
lplatzer <dev@luisplatzer.de>
2022-07-21 15:38:11 +0200

    added form validation, packages

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,68 @@
+import {Box, Button} from "@mui/material";
+import {Formik, Form, Field} from "formik";
+import {TextField} from "formik-mui";
+import * as yup from "yup";
+
+const ContactSchema = yup.object().shape({
+    name: yup.string()
+    .required(),
+    email: yup.string()
+    .email()
+    .required(),
+    message: yup.string()
+    .required()
+})
+
 export default function Contact(){
     return(
-        <h1>contact me</h1>
+        <>
+        <Formik
+            initialValues={{
+                name: '',
+                email: '',
+                message: ''
+            }}
+            validationSchema={ContactSchema}
+            onSubmit={async(values, { setSubmitting }) =>  {
+                setTimeout(() => {
+                    setSubmitting(false);
+                    //todo: add call to contact api (contactForm(values) for example)
+                alert(JSON.stringify(values, null, 3));
+            }, 500);
+            }}
+        >
+            {({submitForm, isSubmitting}) => (
+                <Form>
+                    <Field 
+                    component={TextField}
+                    label="Your name"
+                    id="name"
+                    variant="outlined"
+
+                    />
+                    <Field component={TextField}
+                    label="Your Email"
+                    id="email"
+                    variant="outlined"
+                    />
+                    <Field
+                    component={TextField}
+                    label="Your Message"
+                    id="message"
+                    variant="outlined"
+                    multiline
+                    minRows={4}
+                    />
+                    <Button 
+                        variant="outlined"
+                        color="primary"
+                        onClick={submitForm}
+                    >
+                    Submit
+                    </Button>
+            </Form>
+            )}
+        </Formik>
+        </>
     )
 }
