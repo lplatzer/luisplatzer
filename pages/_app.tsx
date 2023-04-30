00ba1713b00886e5e5b986a158c8f8a9879066f7
Luis <dev@luisplatzer.de>
2023-04-30 22:20:19 +0200

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Head from 'next/head';
-import { AppProps } from 'next/app';
+import type { AppProps } from 'next/app';
+import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
@@ -26,7 +27,8 @@ export default function MyApp(props: MyAppProps) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Layout>
-          <Component {...pageProps} />
+            <Component {...pageProps} />
+            <Analytics/>
         </Layout>
       </ThemeProvider>
     </CacheProvider>
